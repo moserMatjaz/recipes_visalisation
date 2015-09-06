9fd52aef9c37c5678a04b74ea08ae95ff55cd8ec
Matjaz Moser <matjaz.moser@gmail.com>
2015-09-06 15:30:52 +0300

    Organisation changes
    Changes to the file structure and content

--- D3/visualisation.js
+++ D3/visualisation.js
@@ -1,11 +1,11 @@
 var width = 2000;
-var height = 2000;
+var height = 700;
 var svg = d3.select("#map").append("svg").attr("width", width) .attr("height", height);
-var projection = d3.geo.mercator().center([40,35]) .scale(300) .rotate([0,0,0]); 
+var projection = d3.geo.mercator().center([40,35]) .scale(250) .rotate([0,0,0]); 
 var path = d3.geo.path().projection(projection); 
 var g = svg.append("g"); 
 
-var country_selector = d3.select("#map").append("div").attr("class", "country_selector"); 
+var country_selector = d3.select("#map").append("div").attr("class", "country_selector").attr("width", width/10) .attr("height", height/10); 
 
 queue().defer(d3.json, "world-110m.json").defer(d3.csv, "country_data4.csv").await(main);
 
@@ -24,7 +24,7 @@ function main(error, world, countryData){
     });
 
     var colour_scale = d3.scale.linear()
-                        .domain([0,31])
+                        .domain([0,32])
                         .range(["yellow", "black"]);
 
     g.selectAll("path") 
@@ -42,16 +42,14 @@ function main(error, world, countryData){
             .style("opacity", 0.8);
         })
         .on("mouseout", function(d) {
-            country_selector.style("opacity", 0)
-            .style("display", "none");
+            country_selector.style("opacity", 0).style("display", "none");
         })
         .on("mousemove", function(d) {
-            country_selector.style("left", (d3.event.pageX + 7) + "px")
-            .style("top", (d3.event.pageY - 15) + "px");
+            country_selector.style("left", (d3.event.pageX + 7) + "px").style("top", (d3.event.pageY - 15) + "px");
         })
         .on("click", function(d) { 
+            if (c1[d.id] > -1) {
             var current_country = d3.select(this).style("fill", "red");
-
             var country_selected = country_names[d.id];
             console.log(country_selected);
 
@@ -65,8 +63,10 @@ function main(error, world, countryData){
                 type: "POST",
                 accept: "application/json",
                 contentType:"application/json; charset=utf-8",
-                url: "http://localhost:7474/db/data/transaction/commit",
-                headers: {"Authorization":"Basic <bmVvNGo6bW9ycGhsaW5n>"},
+                url: "//recipesdatabasesimilairites.sb02.stations.graphenedb.com:24789/db/data/transaction/commit",
+                //url: "http://localhost:7474/db/data/transaction/commit",
+                headers: {"Authorization":"Basic <cmVjaXBlc19kYXRhYmFzZV9zaW1pbGFpcml0ZXM6ZGJSRnRScXJmMWg1VFpjbHdlMGY=>"},
+                //headers: {"Authorization":"Basic <bmVvNGo6bW9ycGhsaW5n>"},
                 data: JSON.stringify(cypher),
                 success: function(data, textStatus, jqXHR){
                     console.log(data)
@@ -79,22 +79,23 @@ function main(error, world, countryData){
 
                     var paragraph = document.createElement("p");
                     paragraph.id = "paragraph_text";
-                    var text_output = document.getElementById("results");
+                    var text_output = document.getElementById("inside_map");
                     text_output.appendChild(paragraph)
                     
                 for(z = 0; z < 31; z++){ 
                     d3.select("path#_" + data.results[0].data[z].row[2]).style("fill", colour_scale(z)).style("stroke-width", "1px").attr("class", "selected");
                     var name = data.results[0].data[z].row[0];
                     var value = data.results[0].data[z].row[1];
-                    value = value.toFixed(4);
-                    var output = name  + " ----- > " + value;
+                    value = value.toFixed(3);
+                    var output = name  + " - " + value;
                     console.log(output);
                     var div = document.getElementById('paragraph_text');
                     div.innerHTML = div.innerHTML + output + '<br>';
                 };
                 },
                   failure: function(msg){console.log("failed")}
-                });  
+                }); } else { window.alert("NOT IN DATASET. Please choose a shaded country.");
+                            } 
         })
 
         for( k = 0; k < country_IDs.length; k++) {
--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 Description of the repository
   -> Neo4j: all the initial raw data and Cypher queries used in the project
+  		- also uncluded: all the querries performed on the dataset
 
   -> MySQL: SQL statements used to make the DB and all comparisson material + results of comparisson
 
   -> D3: everything that we used for viualisation
+  		- also uncluded: external libraries
 
   -> Scripts: python scripts used for similarity calculation
+  		- also uncluded: data used for calculation
+  		- and raw results
   
   -> report is the overall long description of the project
